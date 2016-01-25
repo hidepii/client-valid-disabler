f37c686dcc5ced16eb516bca9d081b5de6eb82cd
tanaka.hidehito <tanaka.h@smart-up.co.jp>
2016-01-26 01:39:00 +0900

    init

---
+++ ConditionalValidation/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace ConditionalValidation
+{
+    public static class YnnovaHtmlHelper
+    {
+        public static ClientSideValidationDisabler BeginDisableClientSideValidation(this HtmlHelper html)
+        {
+            return new ClientSideValidationDisabler(html);
+        }
+    }
+
+    public class ClientSideValidationDisabler : IDisposable
+    {
+        private HtmlHelper _html;
+
+        public ClientSideValidationDisabler(HtmlHelper html)
+        {
+            _html = html;
+            _html.EnableClientValidation(false);
+        }
+
+        public void Dispose()
+        {
+            _html.EnableClientValidation(true);
+            _html = null;
+        }
+    }
+}
